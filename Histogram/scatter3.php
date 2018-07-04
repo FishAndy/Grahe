41d901464d28a912fcfdd49cf3005f2a20a701cc
FishAndy <40815728+FishAndy@users.noreply.github.com>
2018-07-04 20:42:07 +0800

    2018/7/4網站架構

--- Histogram/scatter3.php
+++ Histogram/scatter3.php
@@ -1,90 +1,173 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>文獻年份散佈圖</title>
-</head>
-<form method="post">
-	<input type="text" name="search" />
-    <input type="submit" value="search" />
-</form>
-<body>
-<div id="container"></div>
-<script src="http://code.highcharts.com/highcharts.js"></script>
-</body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>文獻年份散佈圖</title>
+        <script src="http://code.highcharts.com/highcharts.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <h1><font face="DFKai-sb">生醫物件之視覺化探索</font></h1>
+        <ul class="nav nav-tabs">
+            <li><a href="../104專題生.html">Home</a></li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">搜尋方式<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="../Network/index.php">物件關聯網絡圖</a></li>
+                    <li><a href="">文獻分布關聯圖</a></li>
+                    <li><a href="#">文獻網路圖</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">相關網站<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://ctdbase.org/" target="_blank">Comparative Toxicogenomics Database | CTD</a></li>
+                </ul>
+            </li>
+            <li><a href="../help.html">Help</a></li>
+        </ul>
+        <form method="post">
+            <input type="text" name="search" />
+            <select name="yearone">
+                <option value="1990"SELECTED>1990</option>
+                <option value="1991">1991</option>
+                <option value="1992">1992</option>
+                <option value="1993">1993</option>
+                <option value="1994">1994</option>
+                <option value="1995">1995</option>
+                <option value="1996">1996</option>
+                <option value="1997">1997</option>
+                <option value="1998">1998</option>
+                <option value="1999">1999</option>
+                <option value="2000">2000</option>
+                <option value="2001">2001</option>
+                <option value="2002">2002</option>
+                <option value="2003">2003</option>
+                <option value="2004">2004</option>
+                <option value="2005">2005</option>
+                <option value="2006">2006</option>
+                <option value="2007">2007</option>
+                <option value="2008">2008</option>
+                <option value="2009">2009</option>
+                <option value="2010">2010</option>
+                <option value="2011">2011</option>
+                <option value="2012">2012</option>
+                <option value="2013">2013</option>
+                <option value="2014">2014</option>
+                <option value="2015">2015</option>
+                <option value="2016">2016</option>
+                <option value="2017">2017</option>
+                <option value="2018">2018</option> 
+        	</select>
+         	<font face="DFKai-sb">--></font> 
+            <select name="yeartwo">
+                <option value="1990">1990</option>
+                <option value="1991">1991</option>
+                <option value="1992">1992</option>
+                <option value="1993">1993</option>
+                <option value="1994">1994</option>
+                <option value="1995">1995</option>
+                <option value="1996">1996</option>
+                <option value="1997">1997</option>
+                <option value="1998">1998</option>
+                <option value="1999">1999</option>
+                <option value="2000">2000</option>
+                <option value="2001">2001</option>
+                <option value="2002">2002</option>
+                <option value="2003">2003</option>
+                <option value="2004">2004</option>
+                <option value="2005">2005</option>
+                <option value="2006">2006</option>
+                <option value="2007">2007</option>
+                <option value="2008">2008</option>
+                <option value="2009">2009</option>
+                <option value="2010">2010</option>
+                <option value="2011">2011</option>
+                <option value="2012">2012</option>
+                <option value="2013">2013</option>
+                <option value="2014">2014</option>
+                <option value="2015">2015</option>
+                <option value="2016">2016</option>
+                <option value="2017">2017</option>
+                <option value="2018"SELECTED>2018</option>
+            </select>&nbsp;&nbsp;&nbsp;          
+            <input type="submit" value="search" />
+        </form>
+    	<div id="container"></div>
+    </body>
 </html>
 <?php
 
 if(isset($_POST[search]) && $_POST[search] != NULL)
 {
-	include("../Class/db.class.php");
-	$db = new DB();
-	$x = array();//年
+	include("../Class/db.class.php");$db = new DB();
+		$db = new DB();
+	$x = array();//�~
 	$Objectcase='Disease';
+	$year=$_POST["yeartwo"]-$_POST["yearone"];
+	
 	$Search_Disease = $db->SQL_select("SELECT `DiseaseID`,`DiseaseName` FROM `disease` 
 							  			WHERE `DiseaseID`='$_POST[search]'
 							  			OR `DiseaseName`='$_POST[search]'");
+										
 	if(isset($Search_Disease[0]->DiseaseID) && $Search_Disease[0]->DiseaseID != NULL)
 	{
-		$Ch_Diyear = $db->SQL_select("SELECT `year`,COUNT(`year`)AS`number`
-										 FROM `chemical_disease`,`disease`,`pubmed`
-											 WHERE `disease`.`DiseaseID`='{$Search_Disease[0]->DiseaseID}'
-											  AND `disease`.`DiseaseID`=`chemical_disease`.`DiseaseID`AND `chemical_disease`.`PubMedIDs`=`pubmed`.`PubMedIDs`AND `year`>='1990' AND `year`<='2018'
-											  GROUP BY `year` ORDER BY `year`");
-		$Ge_Diyear = $db->SQL_select("SELECT `year`,COUNT(`year`)AS`number`
-										  FROM `gene_disease`,`disease` ,`pubmed`
-									         WHERE `disease`.`DiseaseID`='{$Search_Disease[0]->DiseaseID}'
-										     AND `disease`.`DiseaseID`=`gene_disease`.`DiseaseID`AND `gene_disease`.`PubMedIDs`=`pubmed`.`PubMedIDs` AND `year`>='1990'AND `year`<='2018'	
-										 GROUP BY `year` ORDER BY `year`");
-	
-    for($i = 0 ; $i <29 ; $i++)
-	{
+	 for($i = 0 ; $i <$year+1 ; $i++)
+	 {
          $x[$i]=array(
 		 'y'=>'',
-         'year'=>1990+$i
+         'year'=>$_POST["yearone"]+$i
           );                 
-	}		
-
-			//print_r($x);			 
-	for($i = 0 ; $i < 29 ; $i++)
-	{
+	 }	
+	 $Ch_Diyear = $db->SQL_select("SELECT `year`,COUNT(`year`)AS`number`
+								   FROM `chemical_disease`,`disease`,`pubmed`
+								   WHERE `disease`.`DiseaseID`='{$Search_Disease[0]->DiseaseID}'
+						           AND `disease`.`DiseaseID`=`chemical_disease`.`DiseaseID`
+								   AND `chemical_disease`.`PubMedIDs`=`pubmed`.`PubMedIDs`
+								   AND `year`>='$_POST[yearone]' AND `year`<='$_POST[yeartwo]'
+								   GROUP BY `pubmed`.`PubMedIDs` ORDER BY `year`");	  
+	 for($i = 0 ; $i < $year+1 ; $i++)
+	 {
 		for($j = 0 ; $j <count($Ch_Diyear) ; $j++)
 		{
-			 echo $x[$i]->year;
+		 // echo $x[$i]->year;
 		  if($x[$i][year]==$Ch_Diyear[$j]->year)
 		  {
-			  $a=$x[$i][y]+$Ch_Diyear[$j]->number;
+			  $a=$x[$i][y]+1;
 			  $x[$i]=array(
 			   'y'=>$a,
 			   'year'=>$x[$i][year]
 			 ); 
-			 break;
-		  }
-		
 			
-	    }
-		              		
-	} 
-	for($i = 0 ; $i < 29 ; $i++)
-	{
+		  }		
+	    }              		
+	 } 	
+	
+	 $Ge_Diyear = $db->SQL_select("SELECT `year`,COUNT(`year`)AS`number`
+								   FROM `gene_disease`,`disease` ,`pubmed`
+								   WHERE `disease`.`DiseaseID`='{$Search_Disease[0]->DiseaseID}'
+								   AND `disease`.`DiseaseID`=`gene_disease`.`DiseaseID`
+								   AND `gene_disease`.`PubMedIDs`=`pubmed`.`PubMedIDs` 
+						           AND `year`>='$_POST[yearone]' AND `year`<='$_POST[yeartwo]'
+							       GROUP BY `pubmed`.`PubMedIDs` ORDER BY `year`");
+		
+	for($i = 0 ; $i < $year+1 ; $i++)
+	 {
 		for($j = 0 ; $j < count($Ge_Diyear) ; $j++)
 		{
 		  if($x[$i][year]==$Ge_Diyear[$j]->year)
 		  {
-			  $a=$x[$i][y]+$Ge_Diyear[$j]->number;
+			  $a=$x[$i][y]+1;
 			   $x[$i]=array(
 			   'y'=>$a,
 			   'year'=>$x[$i][year]
 			 ); 
-			 break;
-		  }
-		  			
-	    }
-		              		
-	}
-	
-	//print_r($x);									 
-	}
+			// break;
+		  }			
+	    }		              		
+	 }	
+	 								 
+}
 }
 ?>
 <script>
@@ -93,7 +176,7 @@ var chart = new Highcharts.Chart({
         renderTo: 'container'
     }, 
    title: {
-        text: '<?php echo $Search_Disease[0]->DiseaseName ?>年分散布圖',
+        text: '<?php echo $Search_Disease[0]->DiseaseName ?>文獻年份散佈圖',
         align: 'center',
         x: 70
     },
--- Network/Index.php
+++ Network/Index.php
@@ -2,7 +2,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>無標題文件</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../Class/vis.js"></script>
+        <script src="networkJS.js"></script>
+        <title>物件關聯網絡圖</title>
     </head>
     <style>
 	#mynetwork {
@@ -11,28 +16,42 @@
 	  border: 1px solid lightgray;
 	}
 	</style>
-    <script type="text/javascript" src="../Class/vis.js"></script>
-   	<script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
-    <script src="networkJS.js"></script>
     <body>
-    <form>
-        ID、Name:<input type="text" id="search" />&nbsp;&nbsp;&nbsp;
-        優先度搜尋:<select id="sel1">
-        	<option value="All">All</option>
-            <option value="Chemical">Chemical</option>
-            <option value="Disease">Disease</option>
-            <option value="Gene">Gene</option>
-        </select>&nbsp;&nbsp;&nbsp;
-        節點個數:<select id="sel2">
-        	<option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5" SELECTED>5</option>
-        </select>&nbsp;&nbsp;&nbsp;
-        <input type="button" value="search" onclick="get_value()"/>
-    </form>
-    <div id="mynetwork"></div>
+        <h1><font face="DFKai-sb">生醫物件之視覺化探索</font></h1>
+        <ul class="nav nav-tabs">
+            <li><a href="../104專題生.html">Home</a></li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">搜尋方式<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="">物件關聯網絡圖</a></li>
+                    <li><a href="../Histogram/scatter3.php">文獻分布關聯圖</a></li>
+                    <li><a href="#">文獻網路圖</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">相關網站<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://ctdbase.org/" target="_blank">Comparative Toxicogenomics Database | CTD</a></li>
+                </ul>
+            </li>
+            <li><a href="../help.html">Help</a></li>
+        </ul>
+        <form>
+            ID、Name:<input type="text" id="search" />&nbsp;&nbsp;&nbsp;
+            優先度搜尋:<select id="sel1">
+                <option value="All">All</option>
+                <option value="Chemical">Chemical</option>
+                <option value="Disease">Disease</option>
+                <option value="Gene">Gene</option>
+            </select>&nbsp;&nbsp;&nbsp;
+            節點個數:<select id="sel2">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5" SELECTED>5</option>
+            </select>&nbsp;&nbsp;&nbsp;
+            <input type="button" value="search" onclick="get_value()"/>
+        </form>
+        <div id="mynetwork"></div>
     </body>
 </html>
 <?
